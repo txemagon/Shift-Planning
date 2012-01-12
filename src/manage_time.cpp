b39a4bf53081d966e3235f966c17cb6287fabb15
txemagon / imasen <txema.gonz@gmail.com>
2012-01-12 02:05:41 +0100

    Some more functions added to refactor the code.

---
+++ src/manage_time.cpp
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  manage_time.cpp
+ *
+ *    Description:  Handle time matters.
+ *
+ *        Version:  1.0
+ *        Created:  11/01/12 22:25:56
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  José M. González (txemagon / imasen), txema.gonz@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+#include "manage_time.h"
+
+enum WeekDay first_day_of_week = monday;	/* Default value */
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  get_day
+ *  Description:  Offsets the week day taking the first_day_of_week into account.
+ * =====================================================================================
+ */
+int
+get_day (int day_of_week)
+{
+  return day_of_week - first_day_of_week;
+}				/* -----  end of function get_day  ----- */
---
+++ src/manage_time.h
@@ -0,0 +1,14 @@
+
+#define		MONDAY  	0
+#define		TUESDAY  	1
+#define		WEDNESDAY	2
+#define		THURSDAY	3
+#define		FRIDAY  	4
+#define		SATURDAY	5
+#define		SUNDAY  	6
+#define		WEEK    	7	/* Number of days in a week */
+
+enum WeekDay
+{ monday, tuesday, wednesday, thursday, friday, saturday, sunday };
+
+extern enum WeekDay first_day_of_week;
