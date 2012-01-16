02def7b8b4a4019b33db0bd8a45bdbc06e52dcf4
txemagon / imasen <txema.gonz@gmail.com>
2012-01-17 00:13:21 +0100

    A Config file with default options
    Inner working of the GA was defined by a header file. So it was necesary
    to recompile the program each time. Now we have a config file in yaml
    format.

---
+++ src/filenames.h
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  filenames.h
+ *
+ *    Description:  File names directory
+ *
+ *        Version:  1.0
+ *        Created:  16/01/12 22:40:13
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  José M. González (txemagon / imasen), txema.gonz@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#ifndef  FILENAMES_H
+#define  FILENAMES_H
+
+
+#define		CONFIG_FILE	"../etc/shift-planning.yml"	/* Params for each run  */
+
+#endif   /* ----- #ifndef FILENAMES_H  ----- */
